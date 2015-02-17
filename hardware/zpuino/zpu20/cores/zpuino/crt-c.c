a7d83ab7c817b1a8a57942b4b506e244019de66c
Alvaro <alvieboy@alvie.com>
2015-02-17 16:43:03 +0000

    Allow proper malloc() when we have memory

--- hardware/zpuino/zpu20/cores/zpuino/crt-c.c
+++ hardware/zpuino/zpu20/cores/zpuino/crt-c.c
@@ -53,8 +53,44 @@ void __attribute__((noreturn)) _premain(void)
 	while(1);
 }
 
-void *sbrk(int inc)
+
+#define PAGESIZE 4096
+static int __brk_initialised=0;
+static void *mend = 0;
+
+static void brk__initialise()
 {
-	start_brk+=inc;
-	return start_brk;
+    unsigned ptr = (unsigned)&__end__;
+    ptr+=(PAGESIZE-1);
+    ptr&=~(PAGESIZE-1);
+
+    mend = (void*)ptr;
+
+    __brk_initialised=1;
+}
+
+void *sbrk(int increment)
+{
+    if (!__brk_initialised)
+        brk__initialise();
+
+    void *ret = (void*)mend;
+
+    increment += (PAGESIZE-1);
+    increment &= (~(PAGESIZE-1));
+
+    mend=(void*)((unsigned char*)mend + increment);
+#ifdef DEBUG_SBRK
+
+    outbyte('S');
+    outbyte('B');
+    outbyte('R');
+    outbyte('K');
+    outbyte(' ');
+    printhex((unsigned)increment);
+    outbyte(' ');
+    printhex((unsigned)ret);
+    outbyte('\n');
+#endif
+    return ret;
 }
