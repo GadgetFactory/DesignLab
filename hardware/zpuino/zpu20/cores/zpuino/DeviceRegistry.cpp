c2da2161159f4ead69c976a96c4d5dfba5b69277
Alvaro <alvieboy@alvie.com>
2015-02-26 18:09:23 +0000

    Add optional debugging

--- hardware/zpuino/zpu20/cores/zpuino/DeviceRegistry.cpp
+++ hardware/zpuino/zpu20/cores/zpuino/DeviceRegistry.cpp
@@ -1,14 +1,27 @@
 #include "DeviceRegistry.h"
 #include "register.h"
+#include <zstdio.h>
 
 namespace ZPUino {
 
     int DeviceRegistry::registerDevice(int slot) {
-        if (slot>=MAX_SLOTS)
+        if (slot>=MAX_SLOTS) {
+#ifdef DEBUG_DEVICEREGISTRY
+            printf("%s: register device on overflow slot %d\r\n",__FUNCTION__,slot);
+#endif
             return -2;
-        if (m_sDeviceRegistry & (1<<slot))
+        }
+        if (m_sDeviceRegistry & (1<<slot)) {
+#ifdef DEBUG_DEVICEREGISTRY
+            printf("%s: cannot register device on slot %d: already registered\r\n",__FUNCTION__,slot);
+#endif
+
             return -1;
+        }
         m_sDeviceRegistry |= (1<<slot);
+#ifdef DEBUG_DEVICEREGISTRY
+        printf("%s: registrer mask is now 0x%08x\r\n",__FUNCTION__,m_sDeviceRegistry);
+#endif
         return 0;
     }
 
@@ -17,6 +30,11 @@ namespace ZPUino {
         for (i=0;i!=16;i++) {
             unsigned val = REGISTER(SYSCTLBASE,16+i);
             if (val!=0) {
+#ifdef DEBUG_DEVICEREGISTRY
+                printf("%s: scanning slot %d for device %04x:%04x.%d: this device is %04x:%04x\r\n",__FUNCTION__,i,(int)vendor,(int)product,(int)instance,
+                      (int)((val>>8)&0xff),(int)(val&0xff)
+                      );
+#endif
                 if (vendor!=VENDOR_ANY) {
                     if (vendor != ((val>>8)&0xff))
                         continue; /* No match */
@@ -27,12 +45,20 @@ namespace ZPUino {
                 }
                 if (isRegistered(i))
                 {
+#ifdef DEBUG_DEVICEREGISTRY
+                printf("%s: skipping slot %d (already registered). Instance now %d.\r\n",__FUNCTION__,i,(int)instance-1);
+#endif
                     --instance;
                     continue;
                 }
 
-                if ((instance==0xff) || (--instance)==0)
+                if ((instance==0xff) || (--instance)==0) {
+#ifdef DEBUG_DEVICEREGISTRY
+                    printf("%s: found device in slot %d (instance now %d).\r\n",__FUNCTION__,i,(int)instance);
+#endif
+
                     return i;
+                }
             }
         }
         return NO_DEVICE;
