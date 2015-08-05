85dd735a2596427ee8649ca6f69834a1050dfd33
Alvaro <alvieboy@alvie.com>
2015-08-05 16:15:15 +0100

    Simple millis() fix

--- hardware/zpuino/zpu20/cores/zpuino/delay.cpp
+++ hardware/zpuino/zpu20/cores/zpuino/delay.cpp
@@ -2,19 +2,28 @@
 
 void delayCycles(unsigned int cycles)
 {
-	unsigned int current = TIMERTSC;
-	unsigned int next = current + cycles;
-	/* This might overflow. We assume cycles is less than 2^30 */
-	if (next<current) {
-		while (TIMERTSC&0x80000000) {}    // Wait for timer to roll over
-	}
+    unsigned int current = TIMERTSC;
+    unsigned int next = current + cycles;
+    /* This might overflow. We assume cycles is less than 2^30 */
+    if (next<current) {
+        while (TIMERTSC&0x80000000) {}    // Wait for timer to roll over
+    }
 
-	while (TIMERTSC<next) {}
+    while (TIMERTSC<next) {}
 }
+
+static unsigned lastMillis = 0;
+static unsigned addMillis = 0;
 
 unsigned int millis(void) {
-    return TIMERTSC / clocksPerMilisecond;
+    unsigned currentMillis= TIMERTSC / clocksPerMilisecond;
+    if (currentMillis<lastMillis) {
+        addMillis+=44739;
+    }
+    lastMillis = currentMillis;
+    return addMillis + currentMillis;
 }
+
 unsigned int micros(void) {
     return TIMERTSC / clocksPerMicrosecond;
 }
